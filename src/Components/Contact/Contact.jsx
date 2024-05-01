027324e8282b5b6bf6b09da096d8cd22ccc5d999
ShahzaibAftab <shahhzaibaftab@gmail.com>
2024-05-01 02:11:10 -0700

    added Projects

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,112 +1,108 @@
-import React, { useRef } from 'react'
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './contact.css'
 
-
 //=======React-icons======
-import {AiFillInstagram} from 'react-icons/ai'
-import {TbArrowBigRightLine} from 'react-icons/tb'
-import {AiFillLinkedin} from 'react-icons/ai'
-
-//=====React emailjs=======
-import emailjs from 'emailjs-com'
+import { AiFillInstagram } from 'react-icons/ai'
+import { TbArrowBigRightLine } from 'react-icons/tb'
+import { AiFillLinkedin } from 'react-icons/ai'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
-
-  const form = useRef();
+  const notify = () => toast.success("Your message has sent!");
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('service_brudfro', 'template_bzwib8n'
-    , form.current, 'QYaif8PXy5MVZBv94')
-      e.target.reset() //reset after submit
-      if(e)
-      {
-        alert('Your messege has been sent');
-      }
-  };
-
+    emailjs.sendForm('service_361gb5k', 'template_kbznz5n', e.target, 'Bs28WcN_Kigd9r5cQ')
+    notify()
+    e.target.reset()
+  }
   return (
-    <section id='Contact' className="contact section container">
-       <div className="sectionTitle">
-        <span className="titleNumber">05.</span>
-        <h5 className="titleText">
-          Contact
-          <div className="underline">
-            <span></span>
-          </div>
-        </h5>
-      </div>
-
-      <div className="contactContainer grid">
-      <h3>Message me</h3>
-        <div className="socialContacts grid">
-          {/* <h3>Talk to me</h3> */}
- 
-          <div className="cards grid">
-            <div className="card">
-              <div>
-            <AiFillInstagram className='icon'></AiFillInstagram>
+    <>
+      <ToastContainer />
+      <section id='Contact' className="contact section container">
+        <div className="sectionTitle">
+          <span className="titleNumber">05.</span>
+          <h5 className="titleText">
+            Contact
+            <div className="underline">
+              <span></span>
             </div>
-             <h4>Instagram</h4>
-             <span className='userName'>@Shahza1baftab</span>
-            <div>
-              <a href='https://www.instagram.com/shahza1baftab/?hl=en' className='flex'>Send messege 
-              <TbArrowBigRightLine className='icon'>
-                </TbArrowBigRightLine></a>
-            </div>
-            </div>
-          </div>
-
-          <div className="cards grid">
-            <div className="card">
-              <div>
-            <AiFillLinkedin className='icon'></AiFillLinkedin>
-            </div>
-             <h4>LinkedIn</h4>
-             <span className='userName'>@Shahzaib Aftab</span>
-            <div>
-              <a href='https://www.linkedin.com/in/shahzaibaftab/?originalSubdomain=pk' className='flex'>Send messege 
-              <TbArrowBigRightLine className='icon'>
-                </TbArrowBigRightLine></a>
-            </div>
-            </div>
-          </div>
-
-           <div className="cards grid">
-            <div className="card">
-              <div>
-            <AiFillInstagram className='icon'></AiFillInstagram>
-            </div>
-             <h4>Email</h4>
-             <span className='userName'>@Shahhzaibaftab@gmail.com</span>
-            <div>
-              <a href='https://mail.google.com/mail/u/0/#inbox?compose=VpCqJZNXSpRmKxCCnRMZTSCQdrSppndrscfZmTHBhsNJZFMDPllfLCdQcfNhSHFsjCqBVGv' className='flex'>Send messege 
-              <TbArrowBigRightLine className='icon'>
-                </TbArrowBigRightLine></a>
-            </div>
-            </div>
-          </div> 
-
+          </h5>
         </div>
 
-<div className="form grid">
-  <h3> Send me an email directly</h3>
-  <form ref={form} onSubmit={sendEmail}>
-    <input type='text' name='name' placeholder='Enter your name'/>
-    
-    <input type='email' name='email' placeholder='Enter your email'/>
+        <div className="contactContainer grid">
+          <h3>Message me</h3>
+          <div className="socialContacts grid">
+            {/* <h3>Talk to me</h3> */}
 
-<textarea name='message' id='' placeholder='Enter your messege'>
+            <div className="cards grid">
+              <div className="card">
+                <div>
+                  <AiFillInstagram className='icon'></AiFillInstagram>
+                </div>
+                <h4>Instagram</h4>
+                <span className='userName'>@Shahza1baftab</span>
+                <div>
+                  <a href='https://www.instagram.com/shahza1baftab/?hl=en' className='flex'>Send messege
+                    <TbArrowBigRightLine className='icon'>
+                    </TbArrowBigRightLine></a>
+                </div>
+              </div>
+            </div>
 
-</textarea>
-<button className='formBtn' type='submit'>
-Send Email
-</button>
-      </form>
-</div>
+            <div className="cards grid">
+              <div className="card">
+                <div>
+                  <AiFillLinkedin className='icon'></AiFillLinkedin>
+                </div>
+                <h4>LinkedIn</h4>
+                <span className='userName'>@Shahzaib Aftab</span>
+                <div>
+                  <a href='https://www.linkedin.com/in/shahzaibaftab/?originalSubdomain=pk' className='flex'>Send messege
+                    <TbArrowBigRightLine className='icon'>
+                    </TbArrowBigRightLine></a>
+                </div>
+              </div>
+            </div>
 
-      </div>
-    </section>
+            <div className="cards grid">
+              <div className="card">
+                <div>
+                  <AiFillInstagram className='icon'></AiFillInstagram>
+                </div>
+                <h4>Email</h4>
+                <span className='userName'>@Shahhzaibaftab@gmail.com</span>
+                <div>
+                  <a href='https://mail.google.com/mail/u/0/#inbox?compose=VpCqJZNXSpRmKxCCnRMZTSCQdrSppndrscfZmTHBhsNJZFMDPllfLCdQcfNhSHFsjCqBVGv' className='flex'>Send messege
+                    <TbArrowBigRightLine className='icon'>
+                    </TbArrowBigRightLine></a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="form grid">
+            <h3> Send me an email directly</h3>
+
+            <form onSubmit={sendEmail}>
+              <input type='text' name='name' id='name' placeholder='Enter your name' />
+
+              <input type='email' name='emailfrom' id='emailfrom' placeholder='Enter your email' />
+
+              <textarea name='message' id='' placeholder='Enter your messege'>
+
+              </textarea>
+              <button className='formBtn' type='submit'>
+                Send Email
+              </button>
+            </form>
+          </div>
+
+        </div>
+      </section>
+    </>
   )
 }
 
